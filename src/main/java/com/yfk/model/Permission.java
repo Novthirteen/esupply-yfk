4cf9135d63b1104149af1bb8829d5929e4952607
druidwang <deluyi@gmail.com>
2015-08-13 15:27:37 +0800

    对象

--- src/main/java/com/yfk/model/Permission.java
+++ src/main/java/com/yfk/model/Permission.java
@@ -11,91 +11,117 @@ import org.springframework.security.core.GrantedAuthority;
 
 @Entity
 @Table(name = "permission")
-public class Permission implements GrantedAuthority {
+public class Permission extends BaseObject {
 
-	 	/**
-	 * 
-	 */
+	/**
+	* 
+	*/
 	private static final long serialVersionUID = 364433575686941032L;
-		private String code;                    // required
-	    private String type;                    // required
-	    private String name;
-	    private String createUser;
-	    private Timestamp createDate;
-	    private String updateUser;
-	    private Timestamp updateDate;
-	    
-	    
-	    
-	    @Id
-	    @Column(length = 50)
-	    public String getCode() {
-	        return this.code;
-	    }
+	private String code; // required
+	private String type; // required
+	private String name;
+	private String createUser;
+	private Timestamp createDate;
+	private String updateUser;
+	private Timestamp updateDate;
 
-	    @Column(length = 1)
-	    public String getType() {
-	        return this.type;
-	    }
-	    
-	    @Column(length = 50)
-	    public String getName() {
-	        return this.name;
-	    }
-
-	    @Column(name = "create_user", length = 50, nullable = false, updatable = false)
-	    public String getCreateUser() {
-	        return createUser;
-	    }
-	    
-	    @Column(name = "create_date", nullable = false, updatable = false)
-	    public Timestamp getCreateDate() {
-	        return createDate;
-	    }
-	    
-	    @Column(name = "update_user", length = 50, nullable = false)
-	    public String getUpdateUser() {
-	        return updateUser;
-	    }
-	    
-	    @Column(name = "update_date", nullable = false)
-	    public Timestamp getUpdateDate() {
-	        return updateDate;
-	    }
-
-	    public void setCode(String code) {
-	        this.code = code;
-	    }
-
-	    public void setName(String name) {
-	        this.name = name;
-	    }
-	    
-	    public void setType(String type) {
-	        this.type = type;
-	    }
-
-
-	    public void setCreateUser(String createUser) {
-	        this.createUser = createUser;
-	    }
-	    
-	    public void setCreateDate(Timestamp createDate) {
-	        this.createDate = createDate;
-	    }
-	    
-	    public void setUpdateUser(String updateUser) {
-	        this.updateUser = updateUser;
-	    }
-	    
-	    public void setUpdateDate(Timestamp updateDate) {
-	        this.updateDate = updateDate;
-	    }
-	    
-	@Override
-	public String getAuthority() {
-		// TODO Auto-generated method stub
-		return null;
+	@Id
+	@Column(length = 50)
+	public String getCode() {
+		return this.code;
 	}
 
+	@Column(length = 1)
+	public String getType() {
+		return this.type;
+	}
+
+	@Column(length = 50)
+	public String getName() {
+		return this.name;
+	}
+
+	@Column(name = "create_user", length = 50, nullable = false, updatable = false)
+	public String getCreateUser() {
+		return createUser;
+	}
+
+	@Column(name = "create_date", nullable = false, updatable = false)
+	public Timestamp getCreateDate() {
+		return createDate;
+	}
+
+	@Column(name = "update_user", length = 50, nullable = false)
+	public String getUpdateUser() {
+		return updateUser;
+	}
+
+	@Column(name = "update_date", nullable = false)
+	public Timestamp getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public void setCreateUser(String createUser) {
+		this.createUser = createUser;
+	}
+
+	public void setCreateDate(Timestamp createDate) {
+		this.createDate = createDate;
+	}
+
+	public void setUpdateUser(String updateUser) {
+		this.updateUser = updateUser;
+	}
+
+	public void setUpdateDate(Timestamp updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((code == null) ? 0 : code.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Permission other = (Permission) obj;
+		if (code == null) {
+			if (other.code != null)
+				return false;
+		} else if (!code.equals(other.code))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Permission [code=" + code + ", type=" + type + ", name=" + name + "]";
+	}
 }
